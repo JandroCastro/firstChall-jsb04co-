f5d2c5329b5e87f7ca123b34d879ebae18dedcc7
Alejandro Castro <55741114+JandroCastro@users.noreply.github.com>
2019-11-15 09:57:40 +0100

    Update banco.js
    Añadir funcionalidades de bloquear cuenta y revisar si el ID del titular coincide con el de la cuenta

--- banco.js
+++ banco.js
@@ -1,8 +1,17 @@
+"use strict";
 class Banco {
   constructor(name, address) {
     this.name = name;
     this.address = address;
     this.clients = [];
+  }
+
+  changeAccountAccess(cuenta) {
+    if (cuenta.access === true) {
+      cuenta.access = false;
+      return;
+    }
+    cuenta.access = true;
   }
 }
 
@@ -10,6 +19,7 @@ class CuentaBancaria {
   constructor(id) {
     this.balance = 0;
     this.id = id;
+    this.access = true;
   }
 }
 
@@ -21,47 +31,60 @@ class Titular {
     this.id = Math.floor(Math.random() * 999999999);
   }
   /*Para abrir la cuenta bancaria. primero "nos damos de alta" 
-como clientes haciendo push al array de clientes del banco
-de un objeto con nuestro nombre e ID. Después generamos una
-instancia de una nueva cuenta, generando otro id*/
+  como clientes haciendo push al array de clientes del banco
+  de un objeto con nuestro nombre e ID. Después generamos una
+  instancia de una nueva cuenta, generando otro id*/
 
   abrirCuentaBancaria(banco) {
     banco.clients.push({ name: this.name, id: this.id });
-    return new CuentaBancaria(Math.floor(Math.random() * 999999999));
+    return new CuentaBancaria(this.id);
   }
   /* Si lo que tengo en el monedero es menor que la cantidad que
-quiero ingresar, me lo alerta. Si es posible hacerlo, actualizamos
-los valores de monedero y cuenta*/
+  quiero ingresar, me lo alerta. Si es posible hacerlo, actualizamos
+  los valores de monedero y cuenta*/
   ingresarDinero(ammount, cuenta) {
-    if (this.moneyPocket > ammount) {
-      cuenta.balance += ammount;
-      this.moneyPocket -= ammount;
-      console.log(
-        `Has ingresado ${ammount} $, tienes ${cuenta.balance} $ en la cuenta, y ${this.moneyPocket} $ en el monedero`
-      );
+    if (cuenta.access === true && cuenta.id === this.id) {
+      if (this.moneyPocket > ammount) {
+        cuenta.balance += ammount;
+        this.moneyPocket -= ammount;
+        console.log(
+          `Has ingresado ${ammount} $, tienes ${cuenta.balance} $ en la cuenta, y ${this.moneyPocket} $ en el monedero`
+        );
+      } else {
+        alert("No tienes suficiente dinero");
+      }
     } else {
-      alert("No tienes suficiente dinero");
+      console.log("No tienes acceso a la cuenta o no coincide con tu id");
     }
   }
+
   /* Si lo que tengo en la cuenta es menor que la cantidad que
-quiero retirar, me lo alerta. Si es posible hacerlo, actualizamos
-los valores de monedero y cuenta*/
+  quiero retirar, me lo alerta. Si es posible hacerlo, actualizamos
+  los valores de monedero y cuenta*/
 
   retirarDinero(ammount, cuenta) {
-    if (cuenta.balance > ammount) {
-      cuenta.balance -= ammount;
-      this.moneyPocket += ammount;
-      console.log(
-        `Has retirado ${ammount} $, te quedan en la cuenta ${cuenta.balance} $`
-      );
+    if (cuenta.access === true && cuenta.id === this.id) {
+      if (cuenta.balance > ammount) {
+        cuenta.balance -= ammount;
+        this.moneyPocket += ammount;
+        console.log(
+          `Has retirado ${ammount} $, te quedan en la cuenta ${cuenta.balance} $`
+        );
+      } else {
+        alert(`Tienes menos de ${ammount} $ en la cuenta`);
+      }
     } else {
-      alert(`Tienes menos de ${ammount} $ en la cuenta`);
+      console.log("No tienes acceso a la cuenta o no coincide con tu ID");
     }
   }
   /*Sacamos por consola el balance de cuenta*/
 
   mostrarSaldo(cuenta) {
-    console.log(`Tienes ${cuenta.balance} $ en la cuenta`);
+    if (cuenta.access === true && cuenta.id === this.id) {
+      console.log(`Tienes ${cuenta.balance} $ en la cuenta`);
+    } else {
+      console.log("No tienes acceso a la cuenta");
+    }
   }
 }
 
@@ -72,9 +95,3 @@ const Jandro = new Titular("Jandro", "Masculino", 3000000);
 const myBank = new Banco("Santander", "Calle Juan Flórez");
 
 const cuentaDeJandro = Jandro.abrirCuentaBancaria(myBank);
-
-Jandro.ingresarDinero(300, cuentaDeJandro);
-Jandro.retirarDinero(100, cuentaDeJandro);
-
-console.log(cuentaDeJandro.balance);
-console.log(Jandro);
